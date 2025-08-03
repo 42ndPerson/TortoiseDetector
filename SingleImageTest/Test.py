004195efa1926fb20724247ebc11d298693df51b
42ndPerson <74685713+42ndPerson@users.noreply.github.com>
2025-08-03 02:12:44 -0600

    Testing

--- SingleImageTest/Test.py
+++ SingleImageTest/Test.py
@@ -9,31 +9,31 @@ device = torch.device(torch_acc_code)
 # Model Summary
 # PE-Core-bigG-14-448 (Checkpoint: meta)
 #   Highly accurate and does not need night correction but is very slow (runs at higher resolution of 448x448)
-#   Catches even obscure tortises and synthetic night tortise
-#   Test with synthetic data suggests that night correction would remove its ability to detect tortises at night
+#   Catches even obscure tortoises and synthetic night tortoise
+#   Test with synthetic data suggests that night correction would remove its ability to detect tortoises at night
 # ViT-g-14 (Checkpoint: laion2b_s34b_b88k)
 #   Intermediate model that somehow performs worse than the light model
-#   Needs night correction to not false positive on all night samples, but likely looses ability to detect night tortises
-#   Cannot detect synthetic night tortise with night correction and sometimes fails at night/day differentiation
+#   Needs night correction to not false positive on all night samples, but likely looses ability to detect night tortoises
+#   Cannot detect synthetic night tortoise with night correction and sometimes fails at night/day differentiation
 # ViT-B-32 (Checkpoint: laion2b_s34b_b79k)
 #   Light model that doesn't seem to produce many false negatives, but produces many false positives
-#   Needs night correction to not false positive on all night samples, but likely looses ability to detect night tortises
+#   Needs night correction to not false positive on all night samples, but likely looses ability to detect night tortoises
 # Note: Forcing a larger image size for smaller models doesn't seem to lead to a improvement at first blush
 
 # Model Setup
-model_name = 'ViT-B-32'#'PE-Core-bigG-14-448'#'ViT-g-14'#'ViT-B-32'
-checkpoint_name = 'laion2b_s34b_b79k'#'meta'#'laion2b_s34b_b88k'#'laion2b_s34b_b79k'
+model_name = 'ViT-g-14'#'PE-Core-bigG-14-448'#'ViT-g-14'#'ViT-B-32'
+checkpoint_name = 'laion2b_s34b_b88k'#'meta'#'laion2b_s34b_b88k'#'laion2b_s34b_b79k'
 model, _, preprocess = open_clip.create_model_and_transforms(model_name, pretrained=checkpoint_name, device=device)
 model.eval()  # model in train mode by default, impacts some models with BatchNorm or stochastic depth active
 tokenizer = open_clip.get_tokenizer(model_name)
 
 # Data
-images = [preprocess(Image.open("Desert.png")).unsqueeze(0), preprocess(Image.open("NightDesert.png")).unsqueeze(0), preprocess(Image.open("Tortise.png")).unsqueeze(0), preprocess(Image.open("ObscureTortise.png")).unsqueeze(0), preprocess(Image.open("ObscureTortise2.png")).unsqueeze(0), preprocess(Image.open("GeminiSyntheticNightTortise.png")).unsqueeze(0)]
-target_text = tokenizer(["empty desert (plants)", "tortise"]).to(device)
+images = [preprocess(Image.open("Desert.png")).unsqueeze(0), preprocess(Image.open("NightDesert.png")).unsqueeze(0), preprocess(Image.open("Tortoise.png")).unsqueeze(0), preprocess(Image.open("ObscureTortoise.png")).unsqueeze(0), preprocess(Image.open("ObscureTortoise2.png")).unsqueeze(0), preprocess(Image.open("GeminiSyntheticNightTortoise.png")).unsqueeze(0)]
+target_text = tokenizer(["empty desert (plants)", "tortoise"]).to(device)
 day_night_text = tokenizer(["day", "night vision"]).to(device)
 
 # Night Correction
-enable_night_correction = True
+enable_night_correction = False
 night_vec = torch.load(f'Average Night Vector ({model_name}).pt').to(device)
 
 with torch.no_grad(), torch.autocast(device_type="mps"):
